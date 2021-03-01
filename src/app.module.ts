c45fad166147028248f81487731970c10a43ab01
Rendy Sutandy <rendy@modalnasional.co.id>
2021-03-01 13:08:20 +0700

    upadate config database nestjs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+
+import configuration from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
