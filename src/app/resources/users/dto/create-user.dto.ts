7007f2ba5e95052a0fbec919b3dfb56ecf25e8a0
Rendy Sutandy <rendy@modalnasional.co.id>
2021-03-04 18:07:17 +0700

    add properti swagger on createUser controler

--- src/app/resources/users/dto/create-user.dto.ts
+++ src/app/resources/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
 import { IsNotEmpty, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
 
+  @ApiProperty()
   @IsNotEmpty()
   @IsEmail()
   fullname: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   email: string;
   
+  @ApiProperty()
   @IsNotEmpty()
   password: string;
 }
