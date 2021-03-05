be8ac29aa2107926ba3ac421d009a7df54086761
Rendy Sutandy <rendy@modalnasional.co.id>
2021-03-05 12:56:10 +0700

    update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Request, Post, Get } from '@nestjs/common';
 import { AuthService } from './middleware/auth/auth.service';
+import { UsersService } from '@user/users/users.service';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private authService: AuthService) { }
+  constructor(
+    private readonly appService: AppService
+  ) { }
 
   @Get()
   getHello(): string {
